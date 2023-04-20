79e667b62560ad9c89dec4c52d258ce601d6922b
Web Dev Cody <webdevcody@gmail.com>
2023-04-20 00:05:25 -0400

    adding in mantime and trying to get dark mode working

---
+++ TODO.md
@@ -0,0 +1,24 @@
+# Features We Need
+
+- create / edit a course landing page
+- a user can create an account (next-auth google authentication)
+- the ability for a course creator to upload video files
+  - title those videos
+  - description on those videos
+- delete a part of the course
+- edit a part of the course
+- stripe payments for a paywall
+
+## Nice to Have Features
+
+- users could have discussion about parts of the course
+- track progress on videos
+- users should be able to navigate forward and backwards through the course
+- refunds
+- coupons for course
+- analytics on user watch time
+- how many users actually finish your course
+- track when people buy courses
+- magic email link authentication
+- analytics on how much money was made daily
+- analytics on how many people viewed your course landing page
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,43 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { useState } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 };
 
